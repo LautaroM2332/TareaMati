4d233004312444ee46e339921cd5d4b6c13d582a
Lautaro Montoya <Lauty.Montoya1@outlook.com>
2024-06-13 17:15:59 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Carrito.cs
@@ -0,0 +1,32 @@
+﻿using TareaMati;
+/*A partir del proyecto "Producto y Carrito" crear en carrito un vector de 10 productos 
+  * en los cuales luego se recorra en un método retornando que hay en ese carrito y cuanta cantidad de ese producto
+  * (mostrar información de los productos)
+  */
+
+Producto producto = new Producto();
+
+producto.productos[0] = "pelota";
+producto.productos[1] = "papitas";
+producto.productos[2] = "auriculares";
+producto.productos[3] = "lapiz";
+producto.productos[4] = "plato";
+producto.productos[5] = "tenedor";
+producto.productos[6] = "cuaderno";
+producto.productos[7] = "goma";
+producto.productos[8] = "droga";
+producto.productos[9] = "merca";
+
+producto.precio[0] = 45;
+producto.precio[1] = 20;
+producto.precio[2] = 60;
+producto.precio[3] = 5;
+producto.precio[4] = 30;
+producto.precio[5] = 12;
+producto.precio[6] = 18;
+producto.precio[7] = 3;
+producto.precio[8] = 35;
+producto.precio[9] = 36;
+
+
+producto.Mostrar();
---
+++ TareaMati/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Serialization;
+
+namespace TareaMati
+{
+    public class Producto
+    {
+        public string[] productos = new string[10];
+        public int[] precio = new int[10];
+       
+        public Producto()
+        {
+
+        }
+
+        public void Mostrar()
+        {
+            for (int i = 0; i < productos.Length; i++) 
+            {
+                Console.WriteLine($"El nombre del producto es {productos[i]}, tiene un precio de ${precio[i]}");
+            }
+            Console.WriteLine($"__________________________\nLa cantidad de productos son: {productos.Length}");
+        }
+    }
+}
